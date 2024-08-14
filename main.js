74846c0e688b8cbde351cc855bd89a6788f62609
Jayden Hendrickse <jayden.heberhendrickse@gmail.com>
2024-08-14 15:42:03 +0200

    figured out menu toggle. Have to fix menu background not extending all the way and switcxh icons

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
-function toggleHamburgerMenu() {
-    
+function toggleMenu() {
+    console.log("Function called")
+    let links = document.querySelector("#nav-links");
+    let nav = document.querySelector(".top-nav");
+    if (links.classList.contains("active") && nav.classList.contains("active")){
+        links.classList.remove("active");
+        nav.classList.remove("active");
+    }
+    else{
+        links.classList.add("active");
+        nav.classList.add("active");
+    }
 }
+
+menuButton = document.querySelector("#menu-icon");
+menuButton.addEventListener("click", toggleMenu);
+console.log(menuButton)
