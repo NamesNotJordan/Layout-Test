f1fd4d0e8c7b4ff43cec1b09754c024d03f3837b
Jayden Hendrickse <jayden.heberhendrickse@gmail.com>
2024-08-14 20:01:32 +0200

    finished the  burger menu. moving on to section 2

--- main.js
+++ main.js
@@ -2,13 +2,16 @@ function toggleMenu() {
     console.log("Function called")
     let links = document.querySelector("#nav-links");
     let nav = document.querySelector(".top-nav");
+    let menuIcon = document.querySelector("i");
     if (links.classList.contains("active") && nav.classList.contains("active")){
         links.classList.remove("active");
         nav.classList.remove("active");
+        menuIcon.classList.replace("fa-xmark", "fa-bars");
     }
     else{
         links.classList.add("active");
         nav.classList.add("active");
+        menuIcon.classList.replace("fa-bars", "fa-xmark");
     }
 }
 
